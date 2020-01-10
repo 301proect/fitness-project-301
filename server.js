7647f2a7b0d1f427c76f435a052cc8263f34dbb7
dantemessy <thepunishar1@gmail.com>
2020-01-10 14:13:48 +0200

    eal api done

--- server.js
+++ server.js
@@ -14,40 +14,88 @@ const client = new pg.Client(DATABASE_URL)
 // call the express in our server
 const server = express();
 server.use(cors());
+// initilizing request method instead of superagent 
+var request = require("request");
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
+
+// middleware 
+server.use(express.urlencoded({ extended: true }));
+
 
 server.set('view engine', 'ejs');
 
 // to use public folder
-server.use( express.static('/public'));
+// server.use( express.static('/public'));
 
 
 // server.listen(PORT,()=>{
 //     console.log(`listening on PORT${PORT}`)
 // })
 
-server.get('/',(req,res)=>{
-    res.status(200).send('hi I am alive')
-})
-server.get('/test',(req,res)=>{
-    res.status(200).send('hi I am alive')
+server.get('/', (req, res) => {
+    res.status(200).render('index')
 })
 
-server.use('*',(req,res)=>{
-    res.status(404).send('nothing there')
+server.get('/newMeal', (req, res) => {
+    res.render('pages/searches/new')
 })
-server.use((error,req,res)=>{
+
+// first route to get the mealplan #1
+server.post('/mealLest', getMeals) ;
+
+function getMeals(req, res) {
+
+    // console.log(req.body)
+    // let time = req.body.time ;
+    let calories = req.body.calories;
+    let dietType = req.body.dietType;
+    let spicial = req.body.spicial;
+
+    var options = {
+        method: 'GET',
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate',
+        qs: {
+            timeFrame: `day`,
+            targetCalories: `${calories}`,
+            diet: `${dietType}`,
+            exclude: `${spicial}`
+        },
+        headers: {
+            'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+            'x-rapidapi-key': 'c3aaa42847msh4114d8320e35996p116efbjsnc1bea287f44f'
+        }
+    }
+
+    request(options, function (error, response, body) {
+        let data = JSON.parse(body);
+        console.log(data);
+        res.render('pages/searches/show', { food: data })
+        if (error) throw new Error(error);
+
+    });
+}
+
+// second route to get exercise
+
+
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).send(' 404 ERROR !!!')
+})
+server.use((error, req, res) => {
     res.status(500).send(error)
 })
 
-client.on('error', error =>{
+client.on('error', error => {
     throw error;
 })
 client.connect()
-    .then(()=>{
-        server.listen(PORT,()=>console.log('server works in DB', PORT))
+    .then(() => {
+        server.listen(PORT, () => console.log('server works in DB', PORT))
     })
-.catch(err =>{
-    throw `Error happened ${err}`;
-})
+    .catch(err => {
+        throw `Error happened ${err}`;
+    })
