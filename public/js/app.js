d10167e056b6509bd043817b5c63efe3f6123277
asharaloran96 <asharoran96@gmail.com>
2020-01-11 21:02:49 +0200

    do the profile page css and jquery version 0.0.2

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,22 @@
-'use strict'
+'use strict';
+    $('#my-food').on('click' , function (val) {
+       if (val === 'default' ){
+        // $('.my-profil').html('');
+        $('.results').hide();
+        $('.exercise-Section').hide();   
+       }else{
+        $('.results').slideToggle(700);   
+       }
 
+    });
+
+    $('#my-exercise').on('click' , function (val) {
+        if (val === 'default'){
+            // $('.my-profil').html('');
+         $('.exercise-Section').hide();  
+         $('.results').hide(); 
+        }else{
+         $('.exercise-Section').slideToggle(700);   
+        }
+ 
+     })
